fac6c564882f589167de63e5bfcbb8afc93188b3
Roman Semchenko <romasavon08@gmail.com>
2025-07-05 12:19:18 +0200

    Created pricing:hero section

---
+++ src/js/pricing.js
@@ -0,0 +1,11 @@
+import '../assets/scss/main.scss'
+import '../assets/scss/pricing.scss'
+
+// components
+import { useTheme } from './components/theme'
+import { useBurger } from './components/burger'
+import { usePriceSwitcher } from './components/swticher'
+
+useTheme()
+useBurger()
+usePriceSwitcher()
